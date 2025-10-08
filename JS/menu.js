8f374c70bd45191ed53c77efb50e89a66f280689
Anv0rax <anvorax@gmail.com>
2025-10-08 23:12:12 +0200

    Center the menu + must be connected to play + escape menu when press ESC + a lot more in menu.js + height 100% -> 100 vh + width 100% -> 100vw - overflow-y overflow-x + ...

--- JS/menu.js
+++ JS/menu.js
@@ -1,13 +1,103 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const startBtn = document.getElementById("start_button");
-  const creditsBtn = document.getElementById("credits_button");
 
-  startBtn.addEventListener("click", () => {
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("game").style.display = "block";
-  });
+  window.onload = function() {
+    const startBtn = document.getElementById("start_button");
+    const creditsBtn = document.getElementById("credits_button");
+    const connectBtn = document.getElementById("connect_button");
+    const settingsBtn = document.getElementById("settings_button");
 
-  creditsBtn.addEventListener("click", () => {
-    window.location.href = "HTML/credits.html";
-  });
+    const divMenu = document.getElementById("menu");
+    const divGame = document.getElementById("game");
+
+    const divEscape = document.getElementById("escape");
+
+    let connected = false;
+
+    divEscape.style.display = "none";
+
+    // if(connected)
+    // {
+    //   divMenu.style.display = "none";
+    //   divGame.style.display = "block";
+    // }
+    // else
+    // {
+    //   divMenu.style.display = "block";
+    //   divGame.style.display = "none";
+    // }
+
+    connectBtn.addEventListener("click", Connect);
+
+    startBtn.addEventListener("click", Start);
+
+    creditsBtn.addEventListener("click", () => {
+      window.location.href = "HTML/credits.html";
+    });
+
+    settingsBtn.addEventListener("click", OpenEscape);
+    
+
+    divEscape.addEventListener("click", GameToMenu);
+
+    document.addEventListener("keydown", PressEscape);
+
+    function Start()
+    {
+      if(connected)
+      {
+        divMenu.style.display = "none";
+        divGame.style.display = "block";
+      }
+      else
+      {
+        NotConnected(); 
+      }
+    }
+
+    function NotConnected() // je crée une fonction car on l'utilisera aussi pour le bouton charger
+    {
+      connectBtn.style.boxShadow = "0px 0px 0px 0.08vw #eb243b"; // code couleur : https://katawashoujo.fandom.com/wiki/Main_Page/Characters
+      connectBtn.style.backgroundColor = "#eb243b";
+    }
+
+    function Connect()
+    {
+      connected = true;
+      if(connected)
+      {
+        connectBtn.textContent = "Vous êtes connecté";
+        connectBtn.style.boxShadow = "0px 0px 0px 0.08vw #2c9e31";
+        connectBtn.style.backgroundColor = "#2c9e31";
+      }
+    }
+
+    function PressEscape(event)
+    {
+      if(event.key == "Escape")
+      {
+        OpenEscape();
+      }
+    }
+
+    function OpenEscape()
+    {
+      if(divEscape.style.display=="none")
+      {
+        divEscape.style.display = "flex";
+
+      }
+      else if(divEscape.style.display=="flex")
+      {
+        divEscape.style.display = "none";
+      }
+    }
+
+    function GameToMenu()
+    {
+      divMenu.style.display = "flex";
+      divGame.style.display = "none";
+      divEscape.style.display = "none"; // supprimer ça après
+    }
+  }
 });
+
