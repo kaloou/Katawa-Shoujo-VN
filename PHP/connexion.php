b9c330fc896062d64b6d596dec4003fcf96b33a2
kalouu <lucaspalu866@gmail.com>
2025-10-19 09:57:09 +0200

    add a config and init php file

--- PHP/connexion.php
+++ PHP/connexion.php
@@ -1,12 +1,9 @@
 <?php
-$serveur_bd = 'student.endor.be';
-$nom_bd = 'js2507';
-$utilisateur_bd = 'js2507';
-$passe_bd = 'daigai28froicla';
-
-$source_bd = "pgsql:host=$serveur_bd;port=5433;dbname=$nom_bd ;sslmode=require";
-$options =  [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,];
-
+/** @var $utilisateur_bd */
+/** @var $passe_bd */
+/** @var $source_bd */
+/** @var $options */
+include('config.php');
 try
 {
     $connexion = new PDO($source_bd, $utilisateur_bd, $passe_bd, $options);
