0df9c67c7102e64524ed13175830838536d4baa6
Anv0rax <anvorax@gmail.com>
2025-11-30 20:35:38 +0100

    fix of the fix of the last merge

--- JS/menu.js
+++ JS/menu.js
@@ -3,16 +3,8 @@ creditsBtn.addEventListener('click', () => {
 	window.location.href = 'HTML/credits.html';
 });
 
-<<<<<<< HEAD
-// el.settingsBtn.addEventListener('click', () => {
-// 	showFlex(el.divEscape);
-// 	blur(el.divMenu);
-// 	blur(el.divGame);
-// });
-=======
 settingsBtn.addEventListener('click', () => {
 	showFlex(divEscape);
 	blur(divMenu);
 	blur(divGame);
 });
->>>>>>> 35c7e66ab6deda2855be7bdfb32ddd880755b4ac
