73fb1f694fd49e24480c06233c4d60d4dc3a72bc
Anv0rax <anvorax@gmail.com>
2025-12-12 22:43:17 +0100

    put session in divGame

--- PHP/play.php
+++ PHP/play.php
@@ -1,44 +1,20 @@
-<!-- 
-            $_SESSION['to_save']['text']['char_name'] = $response['character_name'];
-            $_SESSION['to_save']['text']['char_color'] = $response['character_color'];
-            $_SESSION['to_save']['text']['content'] = $response['text'];
-            $_SESSION['to_save']['text']['char_code'] = $response['character_code'];
-
-
-            $_SESSION['to_save']['bg'] = $response['image_name'];
-
-
-            $_SESSION['to_save']['sprite']['image_name'] = $response['image_name'];
-            $_SESSION['to_save']['sprite']['width'] = $response['width'];
-            $_SESSION['to_save']['sprite']['height'] = $response['height'];
-            $_SESSION['to_save']['sprite']['image_tag'] = $response['image_tag'];
-            $_SESSION['to_save']['sprite']['pos'] = $response['pos'];
-
-
-            $_SESSION['to_save']['sprite'] = [];
-
-
-            $_SESSION['to_save']['centered_text'] = $response['text'];
-
-
-            $_SESSION['to_save']['music'] = $response['music_name'];
--->
-
 <?php
+    session_start();
     try 
     {
-        if(isset($_SESSION["auto_save"]))
+        if(isset($_SESSION["user_id"]))
         {
-            $response["exist"] = true;
-            
+            $response["connected"] = true;
+            // ici include_once('save_to_session'); quand ce sera finit
+            include_once('session_in_game.php');
         }
-        else $response["exist"] = false;
+        else $response["connected"] = false;
         
         echo json_encode($response);
     } 
     catch (PDOException $e) 
     {
-        error_log('get_auto_save.php -> PDOException: ' . $e->getMessage());
+        error_log('play.php -> PDOException: ' . $e->getMessage());
         http_response_code(500);
         echo json_encode(['type' => 'error', 'message' => $e->getMessage()]);
         exit;
